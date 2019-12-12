64aa48c5d92fc121cbe7a5e3cac2fd5196019930
Eran Shaham <shahamer@users.noreply.github.com>
2019-12-12 20:55:07 +0800

    Update RandomBipartiteGraph.java

--- Datasets/random/RandomBipartiteGraph.java
+++ Datasets/random/RandomBipartiteGraph.java
@@ -1,9 +1,3 @@
-// Jan 2016
-// Copyright 2016 Eran SHAHAM, Institute for Infocomm Research (I2R), A*STAR, Singapore 138632.
-// RandomBipartiteGraph is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.   
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// If you wish to use the code, please cite our paper: “On finding the maximum edge biclique in a bipartite graph: a subspace clustering approach”, Eran Shaham, Honghai Yu, Xiao-Li Li.
-
 
 /**
  * Random bipartite graph generator. 
